8a9c4019e2a4842109e8e536b8de960db7073323
iomyaki <io.mknkv@gmail.com>
2025-07-13 22:28:05 +0300

    Fix a bug in the Battleship game

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -11,41 +11,24 @@ from colorama import Fore, init as colorama_init, Style
 AROUND_ALL = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
 AROUND_CROSS = ((0, -1), (-1, 0), (0, 1), (1, 0))
 AROUND_SALTIRE = ((-1, -1), (-1, 1), (1, 1), (1, -1))
-DIRECTIONS = deque(["up", "right", "down", "left"])
+DIRECTIONS = ("up", "right", "down", "left")
 COMPUTER_DEFAULT_TARGETS = deque(
     [
-        [(0, 3), (6, 0)],
-        [(0, 6), (3, 0), (6, 9), (9, 3)],
-        [(3, 9), (9, 6)],
-        [(2, 0), (7, 0), (9, 2)],
-        [(0, 2), (0, 7), (2, 9), (7, 9)],
-        [(0, 4), (9, 4)],
-        [(0, 5), (4, 0), (4, 9), (5, 0), (5, 9), (9, 5)],
-        [(7, 2)],
-        [(7, 7), (9, 7)],
-        [(2, 2), (2, 7)],
-        [(2, 3), (2, 6), (3, 2), (6, 2), (6, 7), (7, 6)],
-        [(3, 7), (7, 3)],
-        [(3, 3), (6, 6)],
-        [(3, 6)],
-        [(1, 2), (2, 1), (2, 8), (7, 1), (8, 2), (8, 7)],
-        [(1, 7), (6, 3), (7, 8)],
-        [(0, 1), (0, 8), (1, 0), (1, 9), (8, 0), (8, 9), (9, 1), (9, 8)],
-        [(2, 5), (4, 7), (5, 7)],
-        [(2, 4), (4, 2), (5, 2), (7, 4), (7, 5)],
-        [(4, 6), (5, 6), (6, 4), (6, 5)],
-        [(3, 4), (3, 5), (4, 3), (5, 3)],
-        [(1, 3), (1, 6), (3, 1)],
-        [(3, 8), (6, 8), (8, 3), (8, 6)],
-        [(6, 1)],
-        [(1, 1)],
-        [(8, 1), (8, 8)],
-        [(1, 8)],
+        [(0, 3), (0, 6), (3, 0), (6, 0), (3, 9), (6, 9), (9, 3), (9, 6)],
+        [(0, 2), (0, 7), (2, 0), (7, 0), (2, 9), (7, 9), (9, 2), (9, 7)],
+        [(0, 4), (0, 5), (4, 0), (5, 0), (4, 9), (5, 9), (9, 4), (9, 5)],
+        [(2, 2), (2, 7), (7, 2), (7, 7)],
+        [(3, 2), (2, 3), (2, 6), (3, 7), (6, 7), (7, 6), (7, 3), (6, 2)],
+        [(3, 3), (3, 6), (6, 3), (6, 6)],
+        [(2, 1), (1, 2), (1, 7), (2, 8), (7, 1), (8, 2), (8, 7), (7, 8)],
+        [(1, 0), (0, 1), (0, 8), (1, 9), (8, 0), (9, 1), (9, 8), (8, 9)],
+        [(2, 4), (2, 5), (4, 7), (5, 7), (7, 4), (7, 5), (5, 2), (4, 2)],
+        [(3, 4), (3, 5), (4, 6), (5, 6), (6, 4), (6, 5), (5, 3), (4, 3)],
+        [(3, 1), (3, 8), (1, 3), (1, 6), (6, 1), (6, 8), (8, 3), (8, 6)],
+        [(1, 1), (1, 8), (8, 1), (8, 8)],
         [(4, 4), (4, 5), (5, 4), (5, 5)],
-        [(4, 1)],
-        [(1, 4), (1, 5), (4, 8), (5, 1), (5, 8), (8, 4), (8, 5)],
-        [(0, 0), (0, 9), (9, 9)],
-        [(9, 0)]
+        [(1, 4), (1, 5), (4, 1), (5, 1), (4, 8), (5, 8), (8, 4), (8, 5)],
+        [(0, 0), (0, 9), (9, 0), (9, 9)],
     ]
 )
 
@@ -247,7 +230,7 @@ class Game:
         )
 
         self.log = Logger(
-            f"battleship/logs/game_{str(datetime.now())[:19].replace(' ', '').replace(':', '').replace('-', '')}.log",
+            f"battleship/logs/game_{str(datetime.now())[:19].replace(' ', '_').replace(':', '_')}.log",
         )
 
     @staticmethod
@@ -516,12 +499,11 @@ class Game:
             else:
                 print(f"{attacker_name} makes its turn…")
                 self.log.write(f"{attacker_name} makes its turn…\n")
-                time.sleep(1.5)
+                time.sleep(1.0)
 
                 if is_seeking:
                     while True:
-                        direction = DIRECTIONS.popleft()
-                        DIRECTIONS.append(direction)
+                        direction = random.choice(DIRECTIONS)
                         if direction in area_of_search and area_of_search[direction]:
                             row, col = seek_the_ship(area_of_search, direction)
                             break
@@ -653,7 +635,7 @@ class Game:
             print(f"Turn {turns_cnt}")
             self.log.write(f"Turn {turns_cnt}\n")
             draw_frame(last_hit)
-            players_order, computer_current_turns, is_seeking, area_of_search, last_hit = make_turn(
+            players_order, computer_current_targets, is_seeking, area_of_search, last_hit = make_turn(
                 players_order,
                 computer_default_targets,
                 computer_current_targets,
